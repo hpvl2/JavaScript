70280abde779422dbc2d336fd4ced57227f930ad
Pavel <hawkrammfan@gmail.com>
2022-05-31 20:32:42 +0300

    install eslint, jest, prettier, husky

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {
+    "import/prefer-default-export": "off",
+    "no-unused-vars": "warn",
+    "max-len": [
+      "error",
+      {
+        code: 120,
+        ignoreComments: true,
+      },
+    ],
+  },
+};
